1b778c53f169a99f0ebbbef2b7f9d74fb4ac1caf
JRLIII24 <ralphyluis44@gmail.com>
2024-11-02 14:43:43 -0700

    income post method

--- Fast-Api/main.py
+++ Fast-Api/main.py
@@ -5,7 +5,7 @@ from routers import expenses, income, bills
 app = FastAPI()
 
 app.include_router(expenses.router)
-# app.include_router(income.router)
+app.include_router(income.router)
 app.include_router(bills.router)
 
 @app.get("/")
--- Fast-Api/routers/income.py
+++ Fast-Api/routers/income.py
@@ -5,3 +5,11 @@ router = APIRouter()
 
 incomes = []
 income_id_count = 1
+
+@router.post("/incomes/", response_model=Income)
+async def add_income(income: Income):
+    global income_id_count
+    income.id = income_id_count
+    incomes.append(income)
+    income_id_count += 1
+    return income
