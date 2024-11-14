a6a94d4edf072fdd2955afed219074ebcc7eb19d
JRLIII24 <ralphyluis44@gmail.com>
2024-11-14 14:58:04 -0800

    Bills and Expenses CRUD

--- Fast-Api/routers/expenses.py
+++ Fast-Api/routers/expenses.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from models import Expense
+from typing import List
 
 router = APIRouter()
+
 
 expenses = []
 expense_id_count = 1
@@ -13,3 +15,36 @@ async def create_expense(expense: Expense):
     expenses.append(expense)
     expense_id_count += 1
     return expense
+
+@router.put("/expenses/{expenses_id}", response_model=Expense)
+async def update_expense(expense_id: int, expense: Expense):
+    for existing_expense in expenses:
+        if existing_expense.id == expense_id:
+            existing_expense.name = expense.name
+            existing_expense.category =expense.category
+            existing_expense.amount = expense.amount
+            existing_expense.date = expense.date
+            return existing_expense
+
+    raise HTTPException(status_code=404, detail="Expense not found")
+
+@router.get("/expenses/", response_model=List[Expense])
+async def get_expenses():
+    return expenses
+
+@router.get("/expenses/{expenses_id}", response_model=Expense)
+async def get_expense_id(expenses_id: int):
+    for e in expenses:
+        if e.id == expenses_id:
+            return e
+    raise HTTPException(status_code=404, detail="Expense not found")
+
+@router.delete("/expenses/{expense_id}")
+async def delete_expense(expenses_id: int):
+    # Find the expense by ID
+    for expense in expenses:
+        if expense.id == expenses_id:
+            expenses.remove(expense)
+            return {"message": "Expense successfully deleted"}
+    # If expense not found, raise a 404 error
+    raise HTTPException(status_code=404, detail="Expense not found")
